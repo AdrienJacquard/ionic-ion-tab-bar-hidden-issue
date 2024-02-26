e473bffd417d539625c1ab8393b581f5fb370524
Adrien Jacquard <39161814+AdrienJacquard@users.noreply.github.com>
2024-02-26 17:36:55 +0000

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tabs-hiding-issue',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
